b026e0873ed9d5ebbe802dbcfaf2fea676f2b3f8
justinguto <jutogustowski@gmail.com>
2026-05-03 16:13:30 -0700

    Implement prime functions

--- main.hpp
+++ main.hpp
@@ -2,51 +2,52 @@
 #include <climits>
 using namespace std;
 
-//****************************
-// Function Prototypes
-//****************************
 void getTwoValues(int &, int &);
 int  isPrime(int);
 int  getNextPrime(int);
 int  getPrevPrime(int);
 
-//****************************
-// getTwoValues()
-// Ask user for two integers. Re-prompt until val1 < val2.
-// No return value.
-//****************************
 void getTwoValues(int &val1, int &val2)
 {
-    // TODO
+    do {
+        cout << "Enter two values (begin < end): ";
+        cin >> val1 >> val2;
+    } while (val1 >= val2);
 }
 
-//****************************
-// isPrime(n)
-// Return 1 if n is a prime number (n >= 2), else 0.
-//****************************
 int isPrime(int n)
 {
-    // TODO
-    return 0;
+    if (n < 2)
+        return 0;
+
+    for (int i = 2; i * i <= n; i++) {
+        if (n % i == 0)
+            return 0;
+    }
+
+    return 1;
 }
 
-//****************************
-// getNextPrime(begin)
-// Return the smallest prime strictly greater than begin.
-//****************************
 int getNextPrime(int begin)
 {
-    // TODO
-    return 0;
+    int p = begin + 1;
+
+    while (isPrime(p) == 0) {
+        p++;
+    }
+
+    return p;
 }
 
-//****************************
-// getPrevPrime(end)
-// Return the largest prime strictly less than end.
-// Return INT_MIN if no such prime exists (end <= 2).
-//****************************
 int getPrevPrime(int end)
 {
-    // TODO
-    return 0;
+    int p = end - 1;
+
+    while (p >= 2) {
+        if (isPrime(p) == 1)
+            return p;
+        p--;
+    }
+
+    return INT_MIN;
 }
